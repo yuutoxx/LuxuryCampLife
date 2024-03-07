f2f760233c55124da30a56e4a3ace8bf639919fb
EC2 Default User <ec2-user@ip-172-31-5-92.ap-northeast-1.compute.internal>
2024-03-07 12:05:32 +0000

    devise設定

---
+++ app/controllers/public/sessions_controller.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+class Public::SessionsController < Devise::SessionsController
+  def after_sign_in_path_for(resource)
+    root_path
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+  # before_action :configure_sign_in_params, only: [:create]
+
+  # GET /resource/sign_in
+  # def new
+  #   super
+  # end
+
+  # POST /resource/sign_in
+  # def create
+  #   super
+  # end
+
+  # DELETE /resource/sign_out
+  # def destroy
+  #   super
+  # end
+
+  # protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  # end
+
+  private
+  # アクティブであるかを判断するメソッド
+  def customer_state
+    # 【処理内容1】 入力されたemailからアカウントを1件取得
+    customer = Customer.find_by(email: params[:customer][:email])
+    # 【処理内容2】 アカウントを取得できなかった場合、このメソッドを終了する
+    return if customer.nil?
+    # 【処理内容3】 取得したアカウントのパスワードと入力されたパスワードが一致していない場合、このメソッドを終了する
+    return unless customer.valid_password?(params[:customer][:password])
+    # 【処理内容4】 アクティブでない会員に対する処理
+    unless customer.is_active
+      flash[:alert] = "すでに退会しています。"
+      redirect_to new_customer_registration_path
+    end
+  end
+end
--- db/migrate/20240307061715_devise_create_customers.rb
+++ db/migrate/20240307061715_devise_create_customers.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-class DeviseCreateUsers < ActiveRecord::Migration[6.1]
+class DeviseCreateCustomers < ActiveRecord::Migration[6.1]
   def change
-    create_table :users do |t|
+    create_table :customers do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
@@ -33,13 +33,15 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       # t.datetime :locked_at
 
       t.string :name
+      t.text :introduction
+      t.boolean :is_active, default: true
 
       t.timestamps null: false
     end
 
-    add_index :users, :email,                unique: true
-    add_index :users, :reset_password_token, unique: true
-    # add_index :users, :confirmation_token,   unique: true
-    # add_index :users, :unlock_token,         unique: true
+    add_index :customers, :email,                unique: true
+    add_index :customers, :reset_password_token, unique: true
+    # add_index :customers, :confirmation_token,   unique: true
+    # add_index :customers, :unlock_token,         unique: true
   end
 end
