9b375f8f0e7cef3a3e90c4fa0a5d19ad3f40594f
EC2 Default User <ec2-user@ip-172-31-5-92.ap-northeast-1.compute.internal>
2024-03-08 10:08:30 +0000

    bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,11 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "jquery";
+import "popper.js";
+import "bootstrap";
+import "../stylesheets/application";
+import "../stylesheets/mystyle.css"
 
 Rails.start()
 Turbolinks.start()
