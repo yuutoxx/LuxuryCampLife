45ae4c511e618ac097dfa3757c840d11a2a8f5f8
EC2 Default User <ec2-user@ip-172-31-5-92.ap-northeast-1.compute.internal>
2024-03-13 14:13:42 +0000

    form,layout

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,7 @@ import "jquery";
 import "popper.js";
 import "bootstrap";
 import "../stylesheets/application";
+import "../preview.js";
 import Raty from "raty.js"
 window.raty = function(elem,opt) {
   let raty = new Raty(elem,opt)
