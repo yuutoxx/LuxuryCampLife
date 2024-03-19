624f9c75b3bd35be50727c46859b0da6731572b3
EC2 Default User <ec2-user@ip-172-31-5-92.ap-northeast-1.compute.internal>
2024-03-19 19:44:35 +0000

    layout

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,5 +1,6 @@
 class Admin::HomesController < ApplicationController
   before_action :authenticate_admin!
   def top
+    @posts = Post.all
   end
 end
--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,5 +1,6 @@
 class Public::HomesController < ApplicationController
   def top
+    @posts = Post.all
     puts"作成したキー #{ENV['SECRET_KEY']}"
   end
 
