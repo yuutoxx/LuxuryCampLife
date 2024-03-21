f66e0258f9dff7eeb29580a8470850a4ef49c278
EC2 Default User <ec2-user@ip-172-31-5-92.ap-northeast-1.compute.internal>
2024-03-21 11:40:32 +0000

    [finish]コメント非同期化

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -4,14 +4,12 @@ class Public::PostCommentsController < ApplicationController
     comment = current_customer.post_comments.new(post_comment_params)
     comment.post_id = post.id
     comment.save
+    @post = Post.find(params[:post_id])
     #redirect_to post_path(post)
   end
 
-  def show
-    @post = Post.find(params[:id])
-  end
-
   def destroy
+    @post = Post.find(params[:post_id])
     PostComment.find(params[:id]).destroy
     #redirect_to post_path(params[:post_id])
   end
